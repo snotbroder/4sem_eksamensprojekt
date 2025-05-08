3e0745abfd492f8c13e80da5f2e11f6947371c4d
Joris <jote0002@stud.kea.dk>
2025-05-08 22:33:52 +0200

    fixet text direction i reversedOrder

--- src/app/admin_components/Section_text_and_image.jsx
+++ src/app/admin_components/Section_text_and_image.jsx
@@ -3,7 +3,7 @@ function Section_text_and_image({ h1, h2, p, btn_txt, btn_href, img, reversedOrd
   return (
     <section className={`flex flex-col md:grid md:grid-cols-2 gap-xs my-lg border-2 border-black ${reversedOrder ? "md:[direction:rtl]" : ""}`}>
       <article className={`flex flex-col bg-primary-500 w-auto aspect-square `}>
-        <div className="my-auto flex flex-col gap-s">
+        <div className={`my-auto flex flex-col gap-s ${reversedOrder ? "md:[direction:ltr]" : ""}`}>
           <h1>{h1}</h1>
           <h2 className="border-y-2 border-darkbrown w-max py-xxs">{h2}</h2>
           <p>{p}</p>
