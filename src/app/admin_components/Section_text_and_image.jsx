e84bca68b878650e130f1a6383e0da67032c7b48
Joris <jote0002@stud.kea.dk>
2025-05-08 21:30:32 +0200

    Section, text w/ image. 1st draft

---
+++ src/app/admin_components/Section_text_and_image.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+function Section_text_and_image({ h1, h2, p, btn_txt, btn_href, img, reversedOrder }) {
+  return (
+    <section className={`flex flex-col md:grid md:grid-cols-2 gap-xs my-lg border-2 border-black ${reversedOrder ? "md:[direction:rtl]" : ""}`}>
+      <article className={`flex flex-col bg-primary-500 w-auto aspect-square `}>
+        <div className="my-auto flex flex-col gap-s">
+          <h1>{h1}</h1>
+          <h2 className="border-y-2 border-darkbrown w-max py-xxs">{h2}</h2>
+          <p>{p}</p>
+          <div className="border-2 border-green-400 p-2 place-self-end">
+            {btn_txt}, {btn_href}, {reversedOrder}
+          </div>
+        </div>
+      </article>
+      <article className="bg-secondary-500 w-auto h-auto aspect-square">
+        <div className="place-self-center mt-xs">
+          <Image src={`/Image_bank/${img}.webp`} width={600} height={600} alt="Building"></Image>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Section_text_and_image;
--- src/app/admin_test/page.js
+++ src/app/admin_test/page.js
@@ -1,13 +1,16 @@
 import Test from "../admin_components/Test";
 import TestVisualizer from "../admin_components/TestVisualizer";
+import Section_text_and_image from "../admin_components/Section_text_and_image";
 
 export default function admin_test() {
   return (
     <div>
       <h1 className="text-3xl">Admin test</h1>
-
-      <Test></Test>
-      <TestVisualizer></TestVisualizer>
+      <Section_text_and_image reversedOrder="" btn_txt="BUTTON TXT" btn_href="https://example.com" img="store_building_facade_stroke_design" h1="Headline 1" h2="Headline 2" p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa quidem quas adipisci. Reprehenderit corporis fugiat ad dolorum alias quis."></Section_text_and_image>
+      <div className="opacity-0">
+        <Test></Test>
+        <TestVisualizer></TestVisualizer>
+      </div>
     </div>
   );
 }
