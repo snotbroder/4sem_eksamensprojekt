d107bc0a3282460fc46060219d25166a77942885
Joris <jote0002@stud.kea.dk>
2025-05-09 12:24:20 +0200

    Tilføjet section_text_background_and_images

---
+++ src/app/admin_components/Section_text_background_and_images.jsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+function Section_text_background_and_images() {
+  // Idé fra codepen: https://codepen.io/intotheprogram/pen/JwWKoP
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const imageList = ["Image_1", "Image_2", "Image_3", "Image_4", "Image_5"];
+    const newImages = imageList.map((name, index) => {
+      const y = Math.floor(Math.random() * 50);
+      const x = Math.floor(Math.random() * 80);
+      return {
+        id: index,
+        src: `/Image_bank/atmosphere/${name}.jpg`,
+        x,
+        y,
+      };
+    });
+    setImages(newImages);
+  }, []);
+
+  return (
+    <section className="h-auto bg-[url(/Image_bank/Lamar_repeating_backdrop_primary500.svg)] bg-bottom-left">
+      <article id="box" className="relative w-full p-xl h-[400px] overflow-hidden">
+        {images.map((image) => (
+          <Image key={image.id} src={image.src} alt="Atmospheric image" style={{ top: `${image.y}%`, left: `${image.x}%` }} width={200} height={200} className="absolute animate-[slow-wiggle_50s_ease_infinite] delay-150"></Image>
+        ))}
+      </article>
+    </section>
+  );
+}
+
+export default Section_text_background_and_images;
--- src/app/admin_test/page.js
+++ src/app/admin_test/page.js
@@ -1,16 +1,18 @@
 import Test from "../admin_components/Test";
 import TestVisualizer from "../admin_components/TestVisualizer";
 import Section_text_and_image from "../admin_components/Section_text_and_image";
+import Section_text_background_and_images from "../admin_components/Section_text_background_and_images";
 
 export default function admin_test() {
   return (
     <div>
       <h1 className="text-3xl">Admin test</h1>
-      <Section_text_and_image reversedOrder="" btn_txt="BUTTON TXT" btn_href="https://example.com" img="store_building_facade_stroke_design" h1="Headline 1" h2="Headline 2" p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa quidem quas adipisci. Reprehenderit corporis fugiat ad dolorum alias quis."></Section_text_and_image>
-      <div className="opacity-0">
+      <Section_text_background_and_images></Section_text_background_and_images>
+      {/* <div className="">
+        <Section_text_and_image reversedOrder="" btn_txt="BUTTON TXT" btn_href="https://example.com" img="store_building_facade_stroke_design" h1="Headline 1" h2="Headline 2" p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa quidem quas adipisci. Reprehenderit corporis fugiat ad dolorum alias quis."></Section_text_and_image>
         <Test></Test>
         <TestVisualizer></TestVisualizer>
-      </div>
+      </div> */}
     </div>
   );
 }
