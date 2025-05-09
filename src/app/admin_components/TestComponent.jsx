7e2ea3c1daaa61298e8cecaab4c3248023abd698
Joris <jote0002@stud.kea.dk>
2025-05-09 13:51:15 +0200

    Created serction breakup w a child component. Implemented changeable motive for backdrop

---
+++ src/app/admin_components/TestComponent.jsx
@@ -0,0 +1,9 @@
+function TestComponent() {
+  return (
+    <article className="bg-secondary-500 w-full h-auto p-10">
+      <h1>Test Component</h1>
+    </article>
+  );
+}
+
+export default TestComponent;
--- src/app/admin_test/page.js
+++ src/app/admin_test/page.js
@@ -2,12 +2,16 @@ import Test from "../admin_components/Test";
 import TestVisualizer from "../admin_components/TestVisualizer";
 import Section_text_and_image from "../admin_components/Section_text_and_image";
 import Section_text_background_and_images from "../admin_components/Section_text_background_and_images";
+import Section_breakup_w_component from "../admin_components/Section_breakup_w_component";
+import TestComponent from "../admin_components/TestComponent";
 
 export default function admin_test() {
   return (
     <div>
       <h1 className="text-3xl">Admin test</h1>
-      <Section_text_background_and_images></Section_text_background_and_images>
+      <Section_breakup_w_component motive="Join">
+        <TestComponent></TestComponent>
+      </Section_breakup_w_component>
       {/* <div className="">
         <Section_text_and_image reversedOrder="" btn_txt="BUTTON TXT" btn_href="https://example.com" img="store_building_facade_stroke_design" h1="Headline 1" h2="Headline 2" p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa quidem quas adipisci. Reprehenderit corporis fugiat ad dolorum alias quis."></Section_text_and_image>
         <Section_text_background_and_images></Section_text_background_and_images>
