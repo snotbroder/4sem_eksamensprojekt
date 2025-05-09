efc0b47dc5b0b9ab6ecf05039891f5494099c3ac
Joris <jote0002@stud.kea.dk>
2025-05-09 09:48:34 +0200

    fjernet baggrundsfarver

--- src/app/admin_components/Section_text_and_image.jsx
+++ src/app/admin_components/Section_text_and_image.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 function Section_text_and_image({ h1, h2, p, btn_txt, btn_href, img, reversedOrder }) {
   return (
     <section className={`flex flex-col md:grid md:grid-cols-2 gap-xs my-lg border-2 border-black ${reversedOrder ? "md:[direction:rtl]" : ""}`}>
-      <article className={`flex flex-col bg-primary-500 w-auto aspect-square `}>
+      <article className={`flex flex-col w-auto aspect-square `}>
         <div className={`my-auto flex flex-col gap-s ${reversedOrder ? "md:[direction:ltr]" : ""}`}>
           <h1>{h1}</h1>
           <h2 className="border-y-2 border-darkbrown w-max py-xxs">{h2}</h2>
@@ -12,7 +12,7 @@ function Section_text_and_image({ h1, h2, p, btn_txt, btn_href, img, reversedOrd
           </div>
         </div>
       </article>
-      <article className="bg-secondary-500 w-auto h-auto aspect-square">
+      <article className=" w-auto h-auto aspect-square">
         <div className="place-self-center mt-xs">
           <Image src={`/Image_bank/${img}.webp`} width={600} height={600} alt="Building"></Image>
         </div>
