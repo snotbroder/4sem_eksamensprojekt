6621ff2d4b1a6a2d633b1bf340b34e99903cfc21
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-12 14:49:08 +0200

    ændrer png til svg

--- src/components/layout/Section_breakup_w_component.jsx
+++ src/components/layout/Section_breakup_w_component.jsx
@@ -1,7 +1,9 @@
 function Section_breakup_w_component({ children, motive }) {
   return (
     <section className="w-full border-2 h-[40rem] border-red-500 bg-[url(/Image_bank/facade_image.jpg)] bg-cover bg-center ">
-      <article className={`h-full bg-[url(/Image_bank/${motive}_repeating_backdrop_secondary500.png)] bg-contain md:bg-cover bg-bottom-right flex flex-col items-center justify-center md:grid md:grid-cols-2 md:gap-xl`}>
+      <article
+        className={`h-full bg-[url(/Image_bank/${motive}_repeating_backdrop_secondary500.svg)] bg-contain md:bg-cover bg-bottom-right flex flex-col items-center justify-center md:grid md:grid-cols-2 md:gap-xl`}
+      >
         <article>{children}</article>
         <article></article>
       </article>
