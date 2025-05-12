ca5d5c940a892d7b5ea3190e54c8de588eaa5ce8
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-12 15:25:59 +0200

    scaffolding på hero sektion

--- src/app/(index)/page.js
+++ src/app/(index)/page.js
@@ -2,11 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col text-green-400  ">
-      <div className="m-auto mt-52 text-4xl">
-        <h1 className="">PERIODT! Babysteps, but we are about to EAAAATTTT</h1>
+    <div className="">
+      <section className="h-[100vh]  bg-[url('/Image_bank/lamar_repeatingtext_secondary500.svg'),_url('/Image_bank/bg-images/heroimage_index.png')]  bg-center bg-cover p-[15vh] relative ">
+        <div>
+          <div className="text-center flex gap-xxs pb-sm absolute right-s">
+            <h4>
+              Gl. Kongevej 27 <br /> 1610 København V
+            </h4>
+            <hr className=" rotate-90 w-m " />
+            <div className="w-fit ">
+              <div className="flex justify-between gap-xs">
+                <h4>Mandag til torsdag</h4>
+                <h4>17-24</h4>
+              </div>
+              <div className="flex justify-between">
+                <h4>Fredag & lørdag</h4>
+                <h4>17.30-24</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="flex flex-col text-green-400  ">
+        <div className="m-auto mt-52 text-4xl">
+          <h1 className="">
+            PERIODT! Babysteps, but we are about to EAAAATTTT
+          </h1>
+        </div>
+        <p className="m-auto text-primary-500">Es hora de comer</p>
       </div>
-      <p className="m-auto text-primary-500">Es hora de comer</p>
     </div>
   );
 }
