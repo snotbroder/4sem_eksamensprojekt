c04736cb5f8de13636f3679b771daa3e9ebd0d12
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-13 15:05:52 +0200

    får hero til at være responsiv og mere readable

--- src/app/(index)/page.js
+++ src/app/(index)/page.js
@@ -3,19 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <section className="h-[100vh]  bg-[url('/Image_bank/lamar_repeatingtext_secondary500.svg'),_url('/Image_bank/bg-images/heroimage_index.png')]  bg-center bg-cover p-[15vh] relative ">
-        <div>
-          <div className="text-center flex gap-xxs pb-sm absolute right-s">
-            <h4>
+      <section className="h-[100vh] lg:bg-[url('/Image_bank/repeatingtext/lamar_repeating_secondary500_herodesktop.svg'),_url('/Image_bank/bg-images/heroimage_index_desktop.png')] bg-[url('/Image_bank/repeatingtext/lamar_repeating_secondary500_heromobile.svg'),_url('/Image_bank/bg-images/heroimage_index_mobile.png')] bg-center   lg:bg-left-bottom bg-cover pt-[12vh] relative grid">
+        <div className="h-full flex flex-col justify-between  lg:justify-end">
+          <div className=" px-s lg:px-6xl lg:flex-row gap-xxs  ">
+            <h4 className="text-right">
               Gl. Kongevej 27 <br /> 1610 København V
             </h4>
-            <hr className=" rotate-90 w-m " />
+            <hr className="collapse lg:visible rotate-90 w-ml self-center " />
             <div className="w-fit ">
               <div className="flex justify-between gap-xs">
                 <h4>Mandag til torsdag</h4>
                 <h4>17-24</h4>
               </div>
-              <div className="flex justify-between">
+              <div className="flex justify-between shadow-beige shadow-6xl">
                 <h4>Fredag & lørdag</h4>
                 <h4>17.30-24</h4>
               </div>
@@ -25,9 +25,7 @@ export default function Home() {
       </section>
       <div className="flex flex-col text-green-400  ">
         <div className="m-auto mt-52 text-4xl">
-          <h1 className="">
-            PERIODT! Babysteps, but we are about to EAAAATTTT
-          </h1>
+          <h1 className="">PERIODT!</h1>
         </div>
         <p className="m-auto text-primary-500">Es hora de comer</p>
       </div>
