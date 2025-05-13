058401424ddebca6284ea7879dc783ac78197c4e
Joris <jote0002@stud.kea.dk>
2025-05-13 09:50:44 +0200

    Optimized used <a> tags to Links from next in dahsboard

--- src/components/dashboard/Dashboard_header.jsx
+++ src/components/dashboard/Dashboard_header.jsx
@@ -1,16 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
 
 import Image from "next/image";
+import Link from "next/link";
 
 function Dashboard_header() {
   return (
     <header className="fixed bg-white w-full h-xl md:h-[90px] flex justify-between border-b-2 border-darkbrown py-xs md:px-lg z-100">
-      <a href="/dashboard" className="flex md:gap-xxs">
+      <Link href="/dashboard" className="flex md:gap-xxs">
         <Image priority src="/Image_bank/logo/logo-red.svg" width={160} height={55} alt="Lamar logo"></Image> <span className="text-[20px] md:leading-[96px] mt-auto uppercase">Dashboard</span>
-      </a>
+      </Link>
       <ul className="flex">
         <li>
-          <a href="/">See website</a>
+          <Link href="/">See website</Link>
         </li>
         <li>
           <UserButton showName />
