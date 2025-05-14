b340680b25a8cbe5bdc50ebd884ac01b756c608b
Joris <jote0002@stud.kea.dk>
2025-05-14 21:21:33 +0200

    Small tweak to dashboard tag, since we updated the globalcss with text padding

--- src/components/dashboard/Dashboard_tag.jsx
+++ src/components/dashboard/Dashboard_tag.jsx
@@ -1,7 +1,7 @@
 function Dashboard_tag({ content, bgColor }) {
   return (
     <div className={`border-[1.5px] border-darkbrown rounded-sm p-[5px] w-fit ${bgColor != "" ? `bg-${bgColor}` : ""}`}>
-      <p className="font-bold">{content == false ? "No content" : content}</p>
+      <span className="font-bold">{content == false ? "No content" : content}</span>
     </div>
   );
 }
