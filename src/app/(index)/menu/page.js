0a745b46a1e776d7aa7183730854abb38273d7f4
Joris <jote0002@stud.kea.dk>
2025-05-14 21:21:47 +0200

    Fetched data successfully

--- src/app/(index)/menu/page.js
+++ src/app/(index)/menu/page.js
@@ -1,21 +1,34 @@
+"use client";
 import MenuCard from "@/components/layout/MenuCard";
 import Button from "../../../components/ui/buttons/Button";
+import { useEffect, useState } from "react";
+import { getAllItems } from "@/app/api";
+import Dashboard_menucard from "@/components/dashboard/Dashboard_menucard";
 
 export default function Menu() {
+  const [menus, setMenus] = useState([]);
+  useEffect(() => {
+    //Kør funktion når kompomenten lades
+    //Fetcher databasens data, og sætter det i lokalt state
+    async function fetchMenus() {
+      const fetchedMenus = await getAllItems("menu-database");
+      setMenus(fetchedMenus);
+    }
+
+    fetchMenus();
+  }, []); //Jeg bruger useEffect fordi ellers renderer komponenten hele tiden. Jeg får den kun til at køre én gang når siden lades
+
   return (
     <div className="">
       <article className="">
-        <div className="   px-s lg:pl-6xl  lg:px-0 grid gap-xs lg:gap-xl  lg:grid-cols-2 lg:items-start">
-          <div className="grid gap-xxs  lg:sticky lg:top-[15%] lg:self-start  ">
+        <div className="   px-s lg:pl-6xl  lg:px-0 grid gap-xs lg:gap-xl  lg:grid-cols-5 lg:items-start">
+          <div className="grid gap-xxs  lg:sticky lg:top-[15%] lg:self-start col-span-2 ">
             <div>
               <h1>Menu</h1>
               <hr></hr>
               <h2>Sharing- & winemenu, a la carte or theatermenu</h2>
               <hr className="bg-darkbrown"></hr>
-              <p className="">
-                The menus has to be ordered for the whole table, and
-                sharedfamily-style. Your food will be served at a fresh pace.
-              </p>
+              <p className="">The menus has to be ordered for the whole table, and sharedfamily-style. Your food will be served at a fresh pace.</p>
             </div>
             <div className="grid gap-xxs">
               <Button variant="secondary">Theatermenu</Button>
@@ -27,7 +40,16 @@ export default function Menu() {
             </div>
           </div>
 
-          <div className="  ">
+          <div className="col-span-3">
+            {menus.length > 0 ? (
+              menus.map((menu) => (
+                <li className="flex flex-col gap-xxs" key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
+                  <Dashboard_menucard menuData={menu} />
+                </li>
+              ))
+            ) : (
+              <p className="animate-pulse">Loading menu...</p>
+            )}
             <article className=" lg:pt-[90px] flex place-content-center  bg-[url('/Image_bank/aboutus/interior1.png')] bg-cover h-[100vh] ">
               <MenuCard className="self-center "></MenuCard>
             </article>
