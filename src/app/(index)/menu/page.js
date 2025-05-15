8933c8f63cc4438eee948c33e4784bb4f7a9fa79
Joris <jote0002@stud.kea.dk>
2025-05-15 16:38:11 +0200

    Added new MenuCard styling. Content fetched from DB

--- src/app/(index)/menu/page.js
+++ src/app/(index)/menu/page.js
@@ -1,5 +1,5 @@
 "use client";
-import MenuCard from "@/components/layout/MenuCard";
+import MenuCard from "@/components/utility/MenuCard";
 import Button from "../../../components/ui/buttons/Button";
 import { useEffect, useState } from "react";
 import { getAllItems } from "@/app/api";
@@ -16,19 +16,19 @@ export default function Menu() {
     }
 
     fetchMenus();
-  }, []); //Jeg bruger useEffect fordi ellers renderer komponenten hele tiden. Jeg får den kun til at køre én gang når siden lades
+  }, []);
 
   return (
-    <div className="">
-      <article className="">
-        <div className="   px-s lg:pl-6xl  lg:px-0 grid gap-xs lg:gap-xl  lg:grid-cols-5 lg:items-start">
-          <div className="grid gap-xxs  lg:sticky lg:top-[15%] lg:self-start col-span-2 ">
-            <div>
-              <h1>Menu</h1>
-              <hr></hr>
-              <h2>Sharing- & winemenu, a la carte or theatermenu</h2>
-              <hr className="bg-darkbrown"></hr>
-              <p className="">The menus has to be ordered for the whole table, and sharedfamily-style. Your food will be served at a fresh pace.</p>
+    <>
+      <article className="overflow-hidden">
+        <div className="grid gap-xs lg:gap-xl  lg:grid-cols-3 lg:items-start">
+          <div className="grid gap-xxs place-items-center  lg:sticky lg:top-[15%] lg:self-start col-span-1 ">
+            <div className="">
+              <h1 className="border-b-2 border-darkbrown">Menu</h1>
+
+              {/* <h2 className="border-y-2 border-darkbrown">Sharing- & winemenu, a la carte or theatermenu</h2> */}
+
+              <p>The menus has to be ordered for the whole table, and sharedfamily-style. Your food will be served at a fresh pace.</p>
             </div>
             <div className="grid gap-xxs">
               <Button variant="secondary">Theatermenu</Button>
@@ -40,31 +40,22 @@ export default function Menu() {
             </div>
           </div>
 
-          <div className="col-span-3">
-            {menus.length > 0 ? (
-              menus.map((menu) => (
-                <li className="flex flex-col gap-xxs" key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
-                  <Dashboard_menucard menuData={menu} />
-                </li>
-              ))
-            ) : (
-              <p className="animate-pulse">Loading menu...</p>
-            )}
-            <article className=" lg:pt-[90px] flex place-content-center  bg-[url('/Image_bank/aboutus/interior1.png')] bg-cover h-[100vh] ">
-              <MenuCard className="self-center "></MenuCard>
-            </article>
-            <article className=" lg:pt-[90px] flex place-content-center  bg-[url('/Image_bank/aboutus/interior1.png')] bg-cover h-[100vh] ">
-              <MenuCard className="self-center "></MenuCard>
-            </article>
-            <article className=" lg:pt-[90px] flex place-content-center  bg-[url('/Image_bank/aboutus/interior1.png')] bg-cover h-[100vh] ">
-              <MenuCard className="self-center "></MenuCard>
+          <div className="col-span-2 overflow-hidden -mx-s ">
+            <article className="place-items-center lg:pt-[90px] flex gap-sm   bg-[url('/Image_bank/wall_of_photos.jpg')] bg-cover h-[100vh] overflow-x-scroll pl-5xl pr-5xl">
+              {menus.length > 0 ? (
+                menus.map((menu) => (
+                  <li className="flex flex-col gap-xxs " key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
+                    {/* <Dashboard_menucard menuData={menu} /> */}
+                    <MenuCard menuData={menu}></MenuCard>
+                  </li>
+                ))
+              ) : (
+                <p className="animate-pulse">Loading menu...</p>
+              )}
             </article>
           </div>
         </div>
       </article>
-      <article>
-        <MenuCard></MenuCard>
-      </article>
-    </div>
+    </>
   );
 }
