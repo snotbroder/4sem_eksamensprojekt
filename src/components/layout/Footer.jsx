3d1efb545282086ea39c45e99d6a7c5cb1496bf0
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-16 20:32:25 +0200

    fjerner den ekstra padding på footer og sætter et par billeder ind i mapper

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -17,9 +17,9 @@ export default function Footer({}) {
           </div>
           <div>
             <h4>Adresse</h4>
-            <p>
+            <span>
               Gl. Kongevej 27 <br></br> 1610 København V
-            </p>
+            </span>
           </div>
         </div>
         <div className="grid lg:flex gap-xs lg:gap-m">
@@ -27,21 +27,19 @@ export default function Footer({}) {
             <h4>Åbningstider</h4>
             <div className="w-fit ">
               <div className="flex justify-between gap-xs">
-                <p>Mandag til torsdag</p>
-                <p className="">17-24</p>
+                <span>Mandag til torsdag</span>
+                <span>17-24</span>
               </div>
               <div className="flex justify-between">
-                <p>Fredag & lørdag</p>
-                <p>17.30-24</p>
+                <span>Fredag & lørdag</span>
+                <span>17.30-24</span>
               </div>
             </div>
           </div>
           <div>
             <h4>Kontakt</h4>
-            <p>
-              <a href="mailto:hej@restaurantlamar.com">hej@restaurantlamar</a>
-              <br></br> <a href="tel:+4526362727">+45 26 36 27 27</a>
-            </p>
+            <a href="mailto:hej@restaurantlamar.com">hej@restaurantlamar</a>
+            <br></br> <a href="tel:+4526362727">+45 26 36 27 27</a>
           </div>
         </div>
       </article>
