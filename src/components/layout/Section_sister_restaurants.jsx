6ffd10231b363dd24617c7dc40dc84bcc4dc584d
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-16 16:21:43 +0200

    linker til de gældende restauranter

--- src/components/layout/Section_sister_restaurants.jsx
+++ src/components/layout/Section_sister_restaurants.jsx
@@ -1,17 +1,45 @@
 import SisterCard from "./cards/SisterCard";
+
 function Section_sister_restaurants() {
   return (
-    <section className="">
+    <section className="mx-s md:mx-6xl">
       <header className="grid">
         <hr></hr>
         <h2>Check out our other restaurants</h2>
         <hr></hr>
       </header>
 
-      <article className="flex  flex-wrap">
-        <SisterCard></SisterCard>
-        <SisterCard></SisterCard>
-        <SisterCard></SisterCard>
+      <article className="flex flex-wrap ">
+        <SisterCard
+          restaurant={"kima"}
+          address={"Frederiksborgvej 2, 2400 København NV"}
+          description={
+            "Kima is our locally rooted, buzzing and cheeky little sister. It's colorful and high-spirited here – both physically and spiritually. We cook food from all corners of the world and serve wine with and without added sulfur and love to guide and talk about wine. Beers on tap, drinks and frothy non-alcoholic alternatives too.."
+          }
+          bgcolor={"secondary-300"}
+          addresscolor={"secondary-600"}
+          href={"/menu"}
+        ></SisterCard>
+        <SisterCard
+          restaurant={"safari"}
+          address={"Baggesensgade 9, 2200 København"}
+          description={
+            "Our cozy modern Italian in Nørrebro, a stone's throw from the lakes. Dive headfirst into pasta, crostini, polenta, risotto and antipasti. The lights are turned down and the sound of the 90s golden age of hip hop and some detours to some of the recent decades are turned up."
+          }
+          bgcolor={"primary-200"}
+          addresscolor={"primary-400"}
+          href={"/menu"}
+        ></SisterCard>
+        <SisterCard
+          restaurant={"omar"}
+          address={"Refsnæsgade 32, 2200 København N"}
+          description={
+            "Omar is the firstborn, the o-g, where it all started and the playground that has layed the foundation for why we do what we do. We celebrate neighborhood, diversity, laissez-faire, beautiful hosting, good taste and festivitas."
+          }
+          bgcolor={"secondary-500"}
+          addresscolor={"secondary-700"}
+          href={"/menu"}
+        ></SisterCard>
       </article>
     </section>
   );
--- src/components/layout/cards/SisterCard.jsx
+++ src/components/layout/cards/SisterCard.jsx
@@ -1,45 +1,56 @@
-function SisterCard({ heading1_stroke, heading2_stroke, heading3_stroke }) {
+import Link from "next/link";
+function SisterCard({
+  restaurant,
+  address,
+  description,
+  bgcolor,
+  addresscolor,
+  href,
+  heading1_stroke,
+  heading2_stroke,
+  heading3_stroke,
+}) {
   return (
-    <section className="relative">
-      <article className="bg-secondary-200 m-xs p-xs w-2/3 lg:w-1/4">
-        <h2 className="sistercardheading text-center">safari</h2>
-
-        <h4 className="text-secondary-600">
-          Frederiksborgvej 2 <br></br>
-          2400 København NV
-        </h4>
-
-        <p>
-          Kima is our locally rooted, buzzing and cheeky little sister. It's
-          colorful and high-spirited here – both physically and spiritually. We
-          cook food from all corners of the world and serve wine with and
-          without added sulfur and love to guide and talk about wine. Beers on
-          tap, drinks and frothy non-alcoholic alternatives too..
-        </p>
-        <p className="place-self-end">Read more</p>
+    <section className=" m-xs  relative w-2/3 lg:w-1/4">
+      <article
+        className={`bg-${bgcolor} w-full h-full p-xs flex flex-col justify-between`}
+      >
+        <div>
+          <h2 className="sistercardheading text-center">{restaurant}</h2>
+          <h4 className={`text-${addresscolor}`}>{address}</h4>
+          <p>{description}</p>
+        </div>
+        <Link
+          className="place-self-end underline decoration-1 underline-offset-4 "
+          href={href}
+        >
+          Read more
+        </Link>
       </article>
-      <article className="place-content-center bg-[url('/Image_bank/bg-images/heroimage_index_desktop.webp')] bg-cover m-xs  w-2/3 lg:w-1/4 text-secondary-500 absolute z-10 h-full flex bottom-0 hover:opacity-0 transition-all cursor-pointer ">
+      <article
+        className={`w-full h-full place-content-center bg-[url('/Image_bank/bg-images/sisterrestaurants/${restaurant}.webp')] bg-cover text-secondary-500 absolute z-10  flex top-0 left-0 hover:opacity-0 transition-all ease-in-out cursor-pointer`}
+      >
         <div className="self-center ">
           <h2
             className={`sistercardheading ${
               heading1_stroke ? "stroke-text" : ""
             }`}
           >
-            safari
+            {restaurant}
           </h2>
           <h2
             className={`sistercardheading ${
               heading2_stroke ? "stroke-text" : ""
             }`}
           >
-            safari
+            {restaurant}
           </h2>
           <h2
             className={`sistercardheading ${
               heading3_stroke ? "stroke-text" : ""
             }`}
           >
-            safari
+            {restaurant}
           </h2>
         </div>
       </article>
