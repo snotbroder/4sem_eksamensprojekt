c29c8e84d2260a2465159adeb253f3c7c7c75925
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-18 14:34:09 +0200

    tilføjer ny komponent med facade og tekst som baggrund

---
+++ src/components/layout/Section_text_and_facade.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+function Section_text_and_facade({}) {
+  return (
+    <section className="relative  ">
+      <Image
+        src="/Image_bank/illustrations/facade1_darkbrown.svg"
+        width={604}
+        height={532}
+        className="absolute left-1/2 top-1/2 translate-[-50%]"
+      ></Image>
+      <Image
+        src="/Image_bank/repeatingtext/mobile/lamar_secondary500.svg"
+        width={393}
+        height={490}
+        className="md:hidden w-full"
+      ></Image>
+      <Image
+        src="/Image_bank/repeatingtext/desktop/lamar_secondary500.svg"
+        width={1280}
+        height={735}
+        className="hidden md:block w-full"
+      ></Image>
+    </section>
+  );
+}
+
+export default Section_text_and_facade;
