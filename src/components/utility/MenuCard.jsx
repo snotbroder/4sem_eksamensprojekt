db149d97a3e18a4b77f0c4d39f466c6406d5b936
Joris <jote0002@stud.kea.dk>
2025-05-18 19:45:20 +0200

    styled menucard with frame

--- src/components/utility/MenuCard.jsx
+++ src/components/utility/MenuCard.jsx
@@ -1,17 +1,20 @@
 function MenuCard({ menuData }) {
   const courseKeys = Object.keys(menuData).filter((key) => key.startsWith("course") && menuData[key]); //Filter gemmem menuData og fin objekter med "course"
   return (
-    <article className="bg-beige border-8 border-darkbrown p-xs md:p-m w-90 md:w-[650px] md:h-[490px] flex flex-col gap-xs overflow-hidden">
-      <header className="flex flex-col gap-xs">
-        <div className={`h-50 md:h-30 bg-${menuData.bgColor} bg-[url('/Image_bank/food_2.svg')] bg-left`}></div>
-        <p>{menuData.menuNote}</p>
+    <article className="bg-beige p-xs md:p-m w-100 md:w-[650px] md:h-[490px] flex flex-col gap-xs overflow-hidden bg-[url('/Image_bank/illustrations/frame-vector-vertical.svg')] md:bg-[url('/Image_bank/illustrations/frame-vector.svg')] bg-no-repeat bg-cover">
+      <header className="flex flex-col gap-xs m-xs">
+        <div className={`h-50 md:h-30 bg-${menuData.bgColor} bg-[url('/Image_bank/food_2.svg')] bg-left border-2 border-darkbrown`}></div>
+        <span>
+          <strong>Menu note:</strong>
+          <p>{menuData.menuNote}</p>
+        </span>
       </header>
-      <main className="border-b-2 border-darkbrown flex justify-between place-items-baseline">
+      <main className="border-b-2 mx-xs border-darkbrown flex justify-between place-items-baseline">
         <h2>{menuData.menuTitle}</h2>
         <h4 className="italic">{menuData.price},-</h4>
       </main>
       <footer>
-        <ul className="md:h-[130px] flex flex-col flex-wrap gap-xxs">
+        <ul className="mx-xs md:h-[130px] flex flex-col flex-wrap gap-xxs">
           {courseKeys.length > 0 ? (
             courseKeys.map((key, index) => (
               <li className="capitalize" key={key}>
