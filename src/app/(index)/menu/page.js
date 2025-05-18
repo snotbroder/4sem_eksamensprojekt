1ec9a25b913cc30a0d97caf0b2408c1bb368e418
Joris <jote0002@stud.kea.dk>
2025-05-18 12:00:23 +0200

    Made scroll and scroll snap work for menu cards in menu subpage

--- src/app/(index)/menu/page.js
+++ src/app/(index)/menu/page.js
@@ -1,9 +1,8 @@
 "use client";
 import MenuCard from "@/components/utility/MenuCard";
 import Button from "../../../components/ui/buttons/Button";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getAllItems } from "@/app/api";
-import Dashboard_menucard from "@/components/dashboard/Dashboard_menucard";
 
 export default function Menu() {
   const [menus, setMenus] = useState([]);
@@ -18,41 +17,65 @@ export default function Menu() {
     fetchMenus();
   }, []);
 
+  // Scroll system
+  const cardRefs = useRef({}); //Lav et tomt object
+
+  function scrollToCard(uuid) {
+    const card = cardRefs.current[uuid];
+    if (card) {
+      card.scrollIntoView({ behavior: "smooth", inline: "start" });
+    }
+  }
+
   return (
     <>
-      <article className="overflow-hidden">
-        <div className="grid gap-xs lg:gap-xl  lg:grid-cols-3 lg:items-start">
-          <div className="grid gap-xxs place-items-center  lg:sticky lg:top-[15%] lg:self-start col-span-1 ">
-            <div className="">
+      <article className="overflow-hidden min-h-screen">
+        {/* Parent container */}
+        <div className="grid gap-xs lg:gap-xl lg:grid-cols-2 xl:grid-cols-3 lg:items-start">
+          {/* Menu buttons and info */}
+          <section className=" grid gap-xxs place-items-center sm-wh lg:sticky  lg:top-[15%] lg:self-start col-span-full lg:col-span-1">
+            <div className="mx-xl ">
               <h1 className="border-b-2 border-darkbrown">Menu</h1>
-
               {/* <h2 className="border-y-2 border-darkbrown">Sharing- & winemenu, a la carte or theatermenu</h2> */}
-
               <p>The menus has to be ordered for the whole table, and sharedfamily-style. Your food will be served at a fresh pace.</p>
             </div>
             <div className="grid gap-xxs">
-              <Button variant="secondary">Theatermenu</Button>
+              {menus.length > 0 ? (
+                menus.map((menu) => (
+                  <li className="flex flex-col gap-xxs" key={menu.uuid}>
+                    <Button onClick={() => scrollToCard(menu.uuid)} variant="secondary">
+                      {menu.menuTitle}
+                    </Button>
+                  </li>
+                ))
+              ) : (
+                <p className="animate-pulse">Loading buttons</p>
+              )}
+              {/* <Button variant="secondary">Theatermenu</Button>
               <Button variant="secondary">Weekendmenu</Button>
-              <Button variant="secondary">Weekdaymenu</Button>
+              <Button variant="secondary">Weekdaymenu</Button> */}
               <Button variant="secondary">A la carte</Button>
               <Button variant="secondary">Drinks & snacks</Button>
               <Button variant="secondary">Winemenu</Button>
             </div>
-          </div>
+          </section>
 
-          <div className="col-span-2 overflow-hidden -mx-s ">
-            <article className="place-items-center lg:pt-[90px] flex gap-sm   bg-[url('/Image_bank/wall_of_photos.jpg')] bg-cover h-[100vh] overflow-x-scroll pl-5xl pr-5xl">
-              {menus.length > 0 ? (
-                menus.map((menu) => (
-                  <li className="flex flex-col gap-xxs " key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
-                    {/* <Dashboard_menucard menuData={menu} /> */}
-                    <MenuCard menuData={menu}></MenuCard>
-                  </li>
-                ))
-              ) : (
-                <p className="animate-pulse">Loading menu...</p>
-              )}
-            </article>
+          {/* Menu cards container */}
+          <div className="col-span-full lg:col-span-1 xl:col-span-2 overflow-hidden">
+            <section className="pl-xl pr-xl md:pr-3xl h-[100vh] pt-[60px] lg:pt-[120px] bg-[url('/Image_bank/wall_of_photos.jpg')] bg-cover">
+              <ul className="snap-x snap-mandatory overflow-x-auto flex gap-xl scroll-smooth scrollbar-none ">
+                {menus.length > 0 ? (
+                  menus.map((menu) => (
+                    <li ref={(el) => (cardRefs.current[menu.uuid] = el)} className="snap-start flex-shrink-0 min-w-[90%] sm:min-w-[70%] md:min-w-[50%] xl:min-w-[33%] flex flex-col gap-xxs" key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
+                      {/* <Dashboard_menucard menuData={menu} /> */}
+                      <MenuCard menuData={menu}></MenuCard>
+                    </li>
+                  ))
+                ) : (
+                  <p className="animate-pulse">Loading menu...</p>
+                )}
+              </ul>
+            </section>
           </div>
         </div>
       </article>
