bda326722f1807b5ae76fdba01a01c4051448a5c
Joris <jote0002@stud.kea.dk>
2025-05-19 13:12:11 +0200

    Added animation to accordion

--- src/components/ui/accordion/AccordionChild.jsx
+++ src/components/ui/accordion/AccordionChild.jsx
@@ -11,32 +11,14 @@ function AccordionChild({ head, copy }) {
     console.log("hi", openAccordion);
   }
   return (
-    <li
-      className="px-xxs border-t-1 border-darkbrown cursor-pointer p-xs"
-      onClick={handleClick}
-    >
+    <li className="px-xxs border-t-1 border-darkbrown cursor-pointer p-xs" onClick={handleClick}>
       <span className="flex justify-between items-center">
         <h4>{head}</h4>
-        <span
-          className={`transition-all duration-200 ease-in-out ${
-            openAccordion && "rotate-135 "
-          }`}
-        >
-          <IconComponent
-            className={`text-darkbrown transition-all duration-200 ease-in-out ${
-              openAccordion && "rotate-135 "
-            }`}
-            icon="add"
-          />
+        <span className={`transition-all duration-200 ease-in-out ${openAccordion && "rotate-135 "}`}>
+          <IconComponent className={`text-darkbrown transition-all duration-200 ease-in-out ${openAccordion && "rotate-90 "}`} icon="add" />
         </span>
       </span>
-      <p
-        className={`transition-all duration-200 ease-in-out overflow-hidden ${
-          openAccordion ? "max-h-40 opacity-100" : "max-h-0 opacity-0 hidden "
-        }`}
-      >
-        {copy}
-      </p>
+      {openAccordion && <p className="slideDown transition-all duration-200 ease-in-out">{copy}</p>}
     </li>
   );
 }
