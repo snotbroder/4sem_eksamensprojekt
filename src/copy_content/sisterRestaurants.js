ff14cc1fef671e7395a21bd6e9661eb3f6a99b2f
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-21 15:05:23 +0200

    sistercards bliver justeret på ift css

--- src/copy_content/sisterRestaurants.js
+++ src/copy_content/sisterRestaurants.js
@@ -2,7 +2,8 @@ var sisterRestaurantData = [
   {
     restaurant: "kima",
     address: "Frederiksborgvej 2, 2400 København NV",
-    description: "Kima is our locally rooted, buzzing and cheeky little sister. It's colorful and high-spirited here. We cook food from all corners of the world and serve wine with and without added sulfur and love to guide and talk about wine. Beers on tap, drinks and frothy non-alcoholic alternatives too..",
+    description:
+      "Kima is our locally rooted, buzzing and cheeky little sister. It's colorful and high-spirited here. We cook food from all corners of the world and serve wine with and without added sulfur and love to guide and talk about wine. Beers on tap, drinks and frothy non-alcoholic alternatives too..",
     bgcolor: "primary-300",
     addresscolor: "secondary-600",
     href: "/menu",
@@ -14,11 +15,12 @@ var sisterRestaurantData = [
   {
     restaurant: "safari",
     address: "Baggesensgade 9, 2200 København",
-    description: "Our cozy modern Italian in Nørrebro. Dive headfirst into pasta, crostini, polenta, risotto and antipasti. The lights are turned down and the sound of the 90s golden age of hip hop and some detours to some of the recent decades are turned up.",
+    description:
+      "Our cozy modern Italian in Nørrebro. Dive headfirst into pasta, crostini, polenta, risotto and antipasti. The lights are turned down and the sound of the 90s golden age of hip hop and some detours to some of the recent decades are turned up.",
     bgcolor: "primary-200",
     addresscolor: "primary-400",
     href: "/menu",
-    // selfend: false,
+    selfend: true,
     heading1_stroke: true,
     heading2_stroke: false,
     heading3_stroke: true,
@@ -26,7 +28,8 @@ var sisterRestaurantData = [
   {
     restaurant: "omar",
     address: "Refsnæsgade 32, 2200 København N",
-    description: "Omar is the firstborn, the o-g, where it all started and the playground that has layed the foundation for why we do what we do. We celebrate neighborhood, diversity, laissez-faire, beautiful hosting, good taste and festivitas.",
+    description:
+      "Omar is the firstborn, the o-g, where it all started and the playground that has layed the foundation for why we do what we do. We celebrate neighborhood, diversity, laissez-faire, beautiful hosting, good taste and festivitas.",
     bgcolor: "primary-500",
     addresscolor: "secondary-700",
     href: "/menu",
