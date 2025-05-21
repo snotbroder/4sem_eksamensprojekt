bb4da61ac58241993cc3c46d33d589c396027ec1
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-21 21:40:44 +0200

    sletter komponent der ik bliver brugt og navngiver komponent til noget andet

--- src/app/(index)/page.js
+++ src/app/(index)/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { FaArrowDown } from "react-icons/fa";
 import Section_text_and_image from "@/components/layout/Section_text_and_image";
-import Section_text_background_and_images2 from "@/components/layout/Section_text_background_and_images2";
+import Section_text_background_and_images from "@/components/layout/Section_text_background_and_images";
 import Section_sister_restaurants from "@/components/layout/Section_sister_restaurants";
 import FoodAndWine_Gif from "/public/Image_bank/gifs/illustration-foodandwine.gif";
 import Facade_Gif from "/public/Image_bank/gifs/illustration-facade.gif";
@@ -63,7 +63,7 @@ export default function Home() {
         </section>
       </Animation>
 
-      <Section_text_background_and_images2></Section_text_background_and_images2>
+      <Section_text_background_and_images></Section_text_background_and_images>
       <section className=" mx-s md:mx-6xl my-3xl">
         <Section_text_and_image
           h1={"About lamar"}
--- src/components/layout/Section_text_background_and_images.jsx
+++ src/components/layout/Section_text_background_and_images.jsx
@@ -3,38 +3,59 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 
 function Section_text_background_and_images() {
-  // Idé fra codepen: https://codepen.io/intotheprogram/pen/JwWKoP
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const imageList = ["Image_1", "Image_2", "Image_3", "Image_4", "Image_5"];
-    const newImages = imageList.map((name, index) => {
-      const y = Math.floor(Math.random() * 100);
-      const x = Math.floor(Math.random() * 80);
-      return {
-        id: index,
-        src: `/Image_bank/atmosphere/${name}.jpg`,
-        x,
-        y,
-      };
-    });
-    setImages(newImages);
-  }, []);
-
   return (
-    <section className="h-[30rem] bg-[url(/Image_bank/Lamar_repeating_backdrop_primary500.svg)] bg-bottom-left  p-lg">
-      <article id="box" className="relative w-full p-xl h-full ">
-        {images.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt="Atmospheric image"
-            style={{ top: `${image.y}%`, left: `${image.x}%` }}
-            width={150}
-            height={150}
-            className="absolute animate-[slow-wiggle_50s_ease_infinite] delay-150"
-          ></Image>
-        ))}
+    <section className=" py-xl ">
+      <article id="box" className="relative   ">
+        <Image
+          src="/Image_bank/aboutus/food2.png"
+          alt="Picture of people talking and food and wine at the table"
+          width={162}
+          height={250}
+          className="absolute -top-lg right-xs lg:w-1/5  lg:object-cover"
+        />
+        <Image
+          src="/Image_bank/atmosphere/Image_4.jpg"
+          alt="Picture of a waiter working"
+          width={120}
+          height={200}
+          className="absolute -bottom-xl left-1/4 md:w-1/5 md:left-4/6 translate-x-[-50%] w-1/4"
+        />
+        <Image
+          src="/Image_bank/aboutus/food1.png"
+          alt="plates of food"
+          width={162}
+          height={200}
+          className="absolute top-lg md::-top-lg md:left-1/5 left-sm  w-1/3 md:w-1/5  "
+        />
+
+        <Image
+          src="/Image_bank/atmosphere/Image_1.jpg"
+          alt="Picture of the restaurants facade"
+          width={152}
+          height={207}
+          className="absolute bottom-1/6 md:w-1/6  right-1/4 md:right-5/6 translate-[50%]"
+        />
+        <Image
+          src="/Image_bank/aboutus/lamarrules.svg"
+          alt="lamar rules"
+          width={209}
+          height={246}
+          className=" absolute left-1/2 top-1/2 translate-[-50%]  w-1/2 md:w-1/4  "
+        />
+        <Image
+          src="/Image_bank/repeatingtext/desktop/lamar_primary500.svg"
+          alt="lamar repeating text desktop"
+          width={1280}
+          height={694}
+          className="hidden md:block relative -z-10 w-full  "
+        />
+        <Image
+          src="/Image_bank/repeatingtext/mobile/lamar_primary500.svg"
+          alt="lamar repeating text mobil"
+          width={393}
+          height={490}
+          className="md:hidden relative -z-10  w-full   "
+        />
       </article>
     </section>
   );
