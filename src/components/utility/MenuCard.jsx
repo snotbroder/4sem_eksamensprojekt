3c4f650cb08b2f784852c7614fb8287be0b3907a
Joris <jote0002@stud.kea.dk>
2025-05-22 17:53:46 +0200

    Small tweaks

--- src/components/utility/MenuCard.jsx
+++ src/components/utility/MenuCard.jsx
@@ -1,13 +1,9 @@
 function MenuCard({ menuData }) {
-  const courseKeys = Object.keys(menuData).filter(
-    (key) => key.startsWith("course") && menuData[key]
-  ); //Filter gemmem menuData og fin objekter med "course"
+  const courseKeys = Object.keys(menuData).filter((key) => key.startsWith("course") && menuData[key]); //Filter gemmem menuData og fin objekter med "course"
   return (
-    <article className="bg-beige p-xs md:p-m w-100 md:w-[650px] md:h-[500px]  flex flex-col gap-xs overflow-hidden bg-[url('/Image_bank/illustrations/frame-vector-vertical.svg')] aspect-[393/626] md:aspect-[465/617] md:bg-[url('/Image_bank/illustrations/frame-vector.svg')]  bg-no-repeat bg-cover ">
+    <article className="bg-beige p-xs md:p-m w-100 md:w-[650px] md:h-[500px] flex flex-col gap-xs overflow-hidden bg-[url('/Image_bank/illustrations/frame-vector-vertical.svg')] aspect-[393/626] md:aspect-[465/617] md:bg-[url('/Image_bank/illustrations/frame-vector.svg')]  bg-no-repeat bg-cover ">
       <header className="flex flex-col gap-xs m-xs">
-        <div
-          className={`h-50 md:h-30 bg-${menuData.bgColor} bg-[url('/Image_bank/illustrations/foodandwine2_darkbrown.svg')] bg-left md:bg-cover border-2 border-darkbrown`}
-        ></div>
+        <div className={`h-50 md:h-30 bg-${menuData.bgColor} bg-[url('/Image_bank/illustrations/foodandwine2_darkbrown.svg')] bg-left md:bg-cover border-2 border-darkbrown`}></div>
         <span>
           <strong>Menu note:</strong>
           <p>{menuData.menuNote}</p>
@@ -18,7 +14,7 @@ function MenuCard({ menuData }) {
         <h2 className="italic">{menuData.price}DKK</h2>
       </main>
       <footer>
-        <ul className="mx-xs md:h-[130px] flex flex-col flex-wrap gap-xxs">
+        <ul className="mx-xs grid grid-cols-2 md:h-[130px] md:flex md:flex-col flex-wrap gap-xxs">
           {courseKeys.length > 0 ? (
             courseKeys.map((key, index) => (
               <li className="capitalize" key={key}>
