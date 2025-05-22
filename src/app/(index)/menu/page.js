389077701a38e54daec0eff10477dff3774f76c8
Joris <jote0002@stud.kea.dk>
2025-05-22 17:53:28 +0200

    Added scroll functions to static menucards. fixed paddingissue with dynamic menucards

--- src/app/(index)/menu/page.js
+++ src/app/(index)/menu/page.js
@@ -14,6 +14,7 @@ import { getAllItems } from "@/app/api";
 
 // Animation
 import Animation from "@/components/aos/Animation";
+import RoutingButton from "@/components/ui/buttons/RoutingButton";
 
 export default function Menu() {
   const [menus, setMenus] = useState([]);
@@ -45,26 +46,17 @@ export default function Menu() {
           {/* Parent container */}
           <div className="grid gap-sm lg:grid-cols-2 xl:grid-cols-5 lg:items-start ">
             {/* Menu buttons and info */}
-            <section
-              data-aos="fade-right"
-              className="mx-sm lg:mx-6xl grid gap-xxs  sm-wh  mt-[18vh] md:self-start col-span-full lg:col-span-1 xl:col-span-2"
-            >
+            <section data-aos="fade-right" className="mx-sm lg:mx-6xl grid gap-xxs  sm-wh  mt-[18vh] md:self-start col-span-full lg:col-span-1 xl:col-span-2">
               <div className=" justify-self-start ">
                 <h1 className="border-b border-darkbrown">Menu</h1>
                 {/* <h2 className="border-y-2 border-darkbrown">Sharing- & winemenu, a la carte or theatermenu</h2> */}
-                <p>
-                  The menus has to be ordered for the whole table, and
-                  sharedfamily-style. Your food will be served at a fresh pace.
-                </p>
+                <p>The menus has to be ordered for the whole table, and sharedfamily-style. Your food will be served at a fresh pace.</p>
               </div>
               <div className="grid gap-xxs">
                 {menus.length > 0 ? (
                   menus.map((menu) => (
                     <li className="flex flex-col gap-xxs" key={menu.uuid}>
-                      <Button
-                        onClick={() => scrollToCard(menu.uuid)}
-                        variant="secondary"
-                      >
+                      <Button onClick={() => scrollToCard(menu.uuid)} variant="secondary">
                         {menu.menuTitle}
                       </Button>
                     </li>
@@ -72,30 +64,25 @@ export default function Menu() {
                 ) : (
                   <p className="animate-pulse">Loading buttons</p>
                 )}
-                {/* <Button variant="secondary">Theatermenu</Button>
-              <Button variant="secondary">Weekendmenu</Button>
-              <Button variant="secondary">Weekdaymenu</Button> */}
-                <Button variant="secondary">A la carte</Button>
-                <Button variant="secondary">Drinks & snacks</Button>
-                <Button variant="secondary">Winemenu</Button>
+                <RoutingButton href="#alacarte" variant="secondary">
+                  A la carte
+                </RoutingButton>
+                <RoutingButton href="#drinks" variant="secondary">
+                  Drinks & snacks
+                </RoutingButton>
+                <RoutingButton href="#winemenu" variant="secondary">
+                  Winemenu
+                </RoutingButton>
               </div>
             </section>
 
             {/* Menu cards container */}
             <div className="col-span-full lg:col-span-1 xl:col-span-3 overflow-hidden bg-[url('/Image_bank/bg-images/wall_of_photos.jpg')] bg-cover pb-2xl">
-              <section className="pl-xl pr-xl md:pr-3xl py-[60px] lg:pt-[120px] ">
-                <ul
-                  data-aos="fade-left"
-                  className="snap-x snap-mandatory overflow-x-auto flex gap-xl scroll-smooth scrollbar-none "
-                >
+              <section className="px-xxs md:px-xs lg:px-lg py-[60px] lg:pt-[120px] ">
+                <ul data-aos="fade-left" className="snap-x snap-mandatory overflow-x-auto flex gap-xl scroll-smooth scrollbar-none ">
                   {menus.length > 0 ? (
                     menus.map((menu) => (
-                      <li
-                        ref={(el) => (cardRefs.current[menu.uuid] = el)}
-                        className="snap-start flex-shrink-0 min-w-[90%] sm:min-w-[70%] md:min-w-[50%] xl:min-w-[33%] flex flex-col gap-xxs"
-                        key={menu.uuid}
-                        onClick={() => clickHandler(menu.uuid)}
-                      >
+                      <li ref={(el) => (cardRefs.current[menu.uuid] = el)} className="snap-start flex-shrink-0 min-w-[90%] sm:min-w-[70%] md:min-w-[50%] xl:min-w-[33%] flex flex-col gap-xxs" key={menu.uuid} onClick={() => clickHandler(menu.uuid)}>
                         {/* <Dashboard_menucard menuData={menu} /> */}
                         <MenuCard menuData={menu}></MenuCard>
                       </li>
@@ -107,7 +94,8 @@ export default function Menu() {
               </section>
             </div>
           </div>
-
+          {/* Scroll to element */}
+          <div className="opacity-0" id="alacarte"></div>
           <section className="grid md:grid-cols-2 gap-3xl md:gap-sm px-sm lg:px-6xl py-3xl bg-[url('/Image_bank/aboutus/spoons.png')] bg-cover ">
             <ClipboardCard bgcolor="secondary-200">
               <article className="  flex flex-col gap-sm md:justify-between h-full ">
@@ -157,9 +145,7 @@ export default function Menu() {
                       <p className="price">85DKK</p>
                     </article>
                     <article className="flex justify-between">
-                      <p>
-                        Banana ice cream, chocolate, crème anglaise & hazelnut
-                      </p>
+                      <p>Banana ice cream, chocolate, crème anglaise & hazelnut</p>
 
                       <p className="price">85DKK</p>
                     </article>
@@ -167,49 +153,35 @@ export default function Menu() {
                 </div>
 
                 <footer>
-                  <Image
-                    src="/Image_bank/illustrations/foodandwine1_darkbrown.svg"
-                    width={100}
-                    height={100}
-                    alt="illustration of food and a person pouring wine"
-                    className="w-full"
-                  ></Image>
+                  <Image src="/Image_bank/illustrations/foodandwine1_darkbrown.svg" width={100} height={100} alt="illustration of food and a person pouring wine" className="w-full"></Image>
                 </footer>
               </article>
             </ClipboardCard>
             <ClipboardCard bgcolor="primary-200">
-              <article className=" grid gap-sm">
+              <article id="drinks" className="grid gap-sm">
                 <header className=" grid pt-xs ">
-                  <h2 className="border-y border-darkbrown ">
-                    Drinks & snacks
-                  </h2>
+                  <h2 className="border-y border-darkbrown ">Drinks & snacks</h2>
                 </header>
                 <main className=" grid gap-sm">
                   <article>
                     <div className="flex justify-between">
                       <div>
                         <p>Gin/tonic</p>
-                        <p className="price -mt-xs">
-                          Gin, classic tonic & lime
-                        </p>
+                        <p className="price -mt-xs">Gin, classic tonic & lime</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Paloma</p>
-                        <p className="price -mt-xs">
-                          Tequila, pink grape & lime
-                        </p>
+                        <p className="price -mt-xs">Tequila, pink grape & lime</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Dark & stormy</p>
-                        <p className="price -mt-xs">
-                          Dark rum, ginger ale & lime
-                        </p>
+                        <p className="price -mt-xs">Dark rum, ginger ale & lime</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
@@ -223,63 +195,49 @@ export default function Menu() {
                     <div className="flex justify-between">
                       <div>
                         <p>Espresso martini</p>
-                        <p className="price -mt-xs">
-                          Vodka, espresso & coffeeliquor
-                        </p>
+                        <p className="price -mt-xs">Vodka, espresso & coffeeliquor</p>
                       </div>
                       <p className="price">100DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Whiskey sour</p>
-                        <p className="price -mt-xs">
-                          Bourbon, citron, angostura bitters, eggwhites & sugar
-                        </p>
+                        <p className="price -mt-xs">Bourbon, citron, angostura bitters, eggwhites & sugar</p>
                       </div>
                       <p className="price">100DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Margarita</p>
-                        <p className="price -mt-xs">
-                          Tequila, triple sec, liquor & lemon
-                        </p>
+                        <p className="price -mt-xs">Tequila, triple sec, liquor & lemon</p>
                       </div>
                       <p className="price">100DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Negroni</p>
-                        <p className="price -mt-xs">
-                          Gin, campari, dolin vermouth & orange
-                        </p>
+                        <p className="price -mt-xs">Gin, campari, dolin vermouth & orange</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Old fashioned</p>
-                        <p className="price -mt-xs">
-                          Bourbon, maraschino-liquor, angostura bitters & orange
-                        </p>
+                        <p className="price -mt-xs">Bourbon, maraschino-liquor, angostura bitters & orange</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Royal grey</p>
-                        <p className="price -mt-xs">
-                          Ginish, earl grey sirup & lemon (alcoholfree)
-                        </p>
+                        <p className="price -mt-xs">Ginish, earl grey sirup & lemon (alcoholfree)</p>
                       </div>
                       <p className="price">85DKK</p>
                     </div>
                     <div className="flex justify-between">
                       <div>
                         <p>Coffee & avec</p>
-                        <p className="price -mt-xs">
-                          Gælder americano eller espresso
-                        </p>
+                        <p className="price -mt-xs">Gælder americano eller espresso</p>
                       </div>
                       <p className="price">80DKK</p>
                     </div>
@@ -329,6 +287,8 @@ export default function Menu() {
               </article>
             </ClipboardCard>
           </section>
+          {/* Scroll to element, virker ikke rigtigt */}
+          <div className="opacity-0" id="winemenu"></div>
           <section className="grid md:grid-cols-2 gap-3xl md:gap-sm px-sm lg:px-6xl py-3xl">
             <ClipboardCard bgcolor="secondary-300">
               <article className="  flex flex-col gap-sm ">
@@ -339,9 +299,7 @@ export default function Menu() {
                   <article className="flex justify-between">
                     <div>
                       <p>Landwein(1 liter) </p>
-                      <p className="price -mt-xs">
-                        Grüner veltliner, Austria, NV
-                      </p>
+                      <p className="price -mt-xs">Grüner veltliner, Austria, NV</p>
                     </div>
                     <p className="price">80 / 450DKK</p>
                   </article>
@@ -390,14 +348,7 @@ export default function Menu() {
                 </main>
               </article>
             </ClipboardCard>
-            <Image
-              data-aos="fade-left"
-              src={Wineshelf_Gif}
-              width={600}
-              height={600}
-              alt="shelf with spinning winebottles and a posterframe "
-              className="self-center"
-            ></Image>
+            <Image data-aos="fade-left" src={Wineshelf_Gif} width={600} height={600} alt="shelf with spinning winebottles and a posterframe " className="self-center"></Image>
           </section>
         </Animation>
       </article>
