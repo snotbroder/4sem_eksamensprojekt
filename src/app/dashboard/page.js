dcf2e8927bd39750267b8330707873f5e50ea22c
Joris <jote0002@stud.kea.dk>
2025-05-25 11:24:26 +0200

    Made card container on index more responsive

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -14,14 +14,14 @@ export default function dashboard() {
 
   useEffect(() => {
     setSelected("home");
-  }, [setSelected]); // safe because this hook always runs now
+  }, [setSelected]);
 
   if (!user) return <SignIn />;
 
   return (
     <>
       <Dashboard_pageName text="home" />
-      <section className="flex flex-col md:flex-row gap-s slideIn">
+      <section className="flex items-center flex-col md:flex-row md:flex-wrap gap-s slideIn">
         <Dashboard_card href="dashboard/menus" btntxt="edit menus" head="Menus" icon="restaurant" copy="Add a menu to the website, or configure an existing one. Change images or delete a menu." />
         <Dashboard_card href="dashboard/bookings" btntxt="see bookings" head="bookings" icon="event" copy="See large groups planned to use the venue. Send emails or cancel the booking" />
         <Dashboard_card href="dashboard/newsletter" btntxt="send a newsletter" head="newsletter" icon="mail" copy="Send newsletters to subscribers. Notify about a new event or an updated season menu." />
