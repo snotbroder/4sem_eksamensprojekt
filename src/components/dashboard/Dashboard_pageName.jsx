c7d441651f547b9877c0ea644a32234a16573269
Joris <jote0002@stud.kea.dk>
2025-05-25 11:37:38 +0200

    Updated style for pageNames to be smaller

--- src/components/dashboard/Dashboard_pageName.jsx
+++ src/components/dashboard/Dashboard_pageName.jsx
@@ -1,9 +1,9 @@
 function Dashboard_pageName({ text }) {
   return (
     <div className="flex overflow-hidden pt-2 mb-sm gap-[-40px] md:gap-xs">
-      <h1 className={`md:!text-[270px] text-darkbrown">{text} ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[270px]"}`}>{text}</h1>
-      <h1 className={`stroke-text opacity-30 md:!text-[270px] text-darkbrown">{text} ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[270px]"}`}>{text}</h1>
-      <h1 className={`stroke-text opacity-30 md:!text-[270px] text-darkbrown">{text} ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[270px]"}`}>{text}</h1>
+      <h1 className={`md:!text-[200px] text-darkbrown">{text} ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[200px]"}`}>{text}</h1>
+      <h1 className={`stroke-text opacity-30 md:!text-[200px] text-darkbrown">{text} ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[200px]"}`}>{text}</h1>
+      <h1 className={`stroke-text opacity-30 md:!text-[200px] ${(text === "newsletter" || text === "bookings") && "!text-[85px] md:!text-[200px]"}`}>{text}</h1>
     </div>
   );
 }
