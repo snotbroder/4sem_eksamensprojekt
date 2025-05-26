1781b83e31f37e17ff5386be8d67a263f86050a6
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-27 00:55:48 +0200

    indsætter nye sektioner

---
+++ src/components/layout/Section_breakup_w_component_contact.jsx
@@ -0,0 +1,14 @@
+function Section_breakup_w_component_contact({ children, motive }) {
+  return (
+    <section className="w-full h-full   ">
+      <article
+        className={`h-full bg-[url(/Image_bank/repeatingtext/mobile/contact_secondary500.svg),_url('/Image_bank/aboutus/interior3.png')] md:bg-[url(/Image_bank/repeatingtext/desktop/contact_secondary500.svg),_url('/Image_bank/aboutus/interior3.png')] bg-cover  bg-center flex flex-col items-center justify-center md:grid md:grid-cols-2 md:gap-xl px-sm lg:px-6xl py-3xl bg-fixed md:bg-fixed`}
+      >
+        <article data-aos="fade-up">{children}</article>
+        <article></article>
+      </article>
+    </section>
+  );
+}
+
+export default Section_breakup_w_component_contact;
---
+++ src/copy_content/accordionContact.js
@@ -0,0 +1,16 @@
+var accordionContentContact = [
+  {
+    head: "Opening hours",
+    copy: "Monday to Thursday: 17-24 Friday & Saturday 17.30-24",
+  },
+  {
+    head: "Address",
+    copy: "Gl. Kongevej 27 1610 København V",
+  },
+  {
+    head: "Contact",
+    copy: "hej@restaurantlamar.com +45 26 36 27 27",
+  },
+];
+
+export default accordionContentContact;
