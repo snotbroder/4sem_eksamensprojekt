a25cadbc6e35beca5aa63cee74287058d2f0937e
Sofia Weng <sowe0001@stud.kea.dk>
2025-05-26 11:08:09 +0200

    test

--- src/app/(index)/faq/page.js
+++ src/app/(index)/faq/page.js
@@ -1,70 +1,71 @@
 import AccordionParent from "../../../components/ui/accordion/AccordionParent";
 import accordionContentFAQ from "/src/copy_content/accordionFAQ.js";
 import Head from "next/head";
-export default function Faq() {
-  const faqJsonLd = {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    mainEntity: [
-      {
-        "@type": "Question",
-        name: "How does your menu works?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "All our menus are sharing dishes and must be ordered by the entire table. We also offer a la carte, where we recommend 2-3 dishes per person.",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "Do I need to show a theater ticket for the theatermenu?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "It is not necessary to show a theater ticket to order our theater menu.",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "What if I'm a party of more than 9 people?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "At our restaurant we warmly welcome larger parties of 9 people or more. Whether it's a birthday, company party, anniversary or another special occasion, we are ready to create a festive evening for you and your guests. Contact us today to hear more about the possibilities, and let us help plan your next event. We look forward to being a part of your big night.",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "Are you hiring?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "You can always apply unsolicited by sending us your CV by email. We are always open to new people in our family.",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "Where are you located?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "Gl. Kongevej 27, 1610 København V",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "Opening hours & public holiday",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "Monday to Thursday: 17-24 and Friday and Saturday: 17.30-24. We are open every public holiday except Christmas Eve.",
-        },
-      },
-      {
-        "@type": "Question",
-        name: "Can I buy a giftcard?",
-        acceptedAnswer: {
-          "@type": "Answer",
-          text: "Yes, we also sell giftcards – which you can buy here https://www.restaurantlamar.com/gavekort",
-        },
-      },
-    ],
-  };
 
+export const faqJsonLd = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  mainEntity: [
+    {
+      "@type": "Question",
+      name: "How does your menu works?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "All our menus are sharing dishes and must be ordered by the entire table. We also offer a la carte, where we recommend 2-3 dishes per person.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Do I need to show a theater ticket for the theatermenu?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "It is not necessary to show a theater ticket to order our theater menu.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "What if I'm a party of more than 9 people?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "At our restaurant we warmly welcome larger parties of 9 people or more. Whether it's a birthday, company party, anniversary or another special occasion, we are ready to create a festive evening for you and your guests. Contact us today to hear more about the possibilities, and let us help plan your next event. We look forward to being a part of your big night.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Are you hiring?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "You can always apply unsolicited by sending us your CV by email. We are always open to new people in our family.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Where are you located?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Gl. Kongevej 27, 1610 København V",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Opening hours & public holiday",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Monday to Thursday: 17-24 and Friday and Saturday: 17.30-24. We are open every public holiday except Christmas Eve.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Can I buy a giftcard?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Yes, we also sell giftcards – which you can buy here https://www.restaurantlamar.com/gavekort",
+      },
+    },
+  ],
+};
+
+export default function Faq() {
   return (
     <>
       <Head>
