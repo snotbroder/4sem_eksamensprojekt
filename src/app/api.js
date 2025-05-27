4b281afc98ec0e69b9d4c4d947f9a17a3b1dd40d
Joris <jote0002@stud.kea.dk>
2025-05-27 23:34:13 +0200

    fjernet kommentar

--- src/app/api.js
+++ src/app/api.js
@@ -6,10 +6,6 @@ export const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 // const url = "https://cckofydhouffprwkdcpw.supabase.co/rest/v1/";
 // const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNja29meWRob3VmZnByd2tkY3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzMzMjEsImV4cCI6MjA2MDU0OTMyMX0.lHyOFxpm9qFmThwVLO2SCLRNnGTsTIaiaF4Zq6zzGYI";
 
-// export async function getTest() {
-//   console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY", url);
-// }
-
 //Tilføj data til databasen
 export async function addItem(itemData, tableName) {
   let headersList = {
